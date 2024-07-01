6e5c982d1587f1886bee63d72a0564076384f30a
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-07-01 12:16:53 -0600

    Modify hero

--- src/components/CounterDown.jsx
+++ src/components/CounterDown.jsx
@@ -3,7 +3,7 @@ import { useCountdown } from '../hooks/useCounterDown'
 export function CounterDown({ i18n }) {
   const { days, hours, minutes, seconds } = useCountdown('5 sept 2024 3:00:00')
   return (
-    <div className='text-white bg-transparent bg-opacity-50 backdrop-blur-md flex text-center justify-center gap-3 text-sm font-semibold py-5 px-5 border rounded-lg mt-10 w-full max-w-[500px]'>
+    <div className='text-white bg-transparent bg-opacity-50 backdrop-blur-md flex text-center justify-center gap-3 text-sm font-semibold py-5 px-5 border rounded-lg mt-4 w-full max-w-[500px]'>
       <div>
         <span className='font-extrabold  text-4xl lg:text-5xl'>{days}</span>{' '}
         {i18n.days}
