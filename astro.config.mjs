55257d327763ceb3668f7d4cd78df2adb63fe8ba
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-07-24 16:47:43 -0600

    feat: Add seo to site web

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,23 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://foro-nearshoring.igeco.mx',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap({
+    filter: (page) => 
+      page !== 'https://foro-nearshoring.igeco.mx/gracias-por-registrarte/' &&
+      page !== 'https://foro-nearshoring.igeco.mx/en/thanks-for-registering/',
+    i18n: {
+      defaultLocale: "es",
+      locales: {
+        es: 'es-ES',
+        en: 'en-US'
+      }
+    }
+  })],
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"]
