c3c22bfd21f39ab9b65841e8517b73f58a01ea90
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-07-24 16:03:09 -0600

    Update FocusSegments component

--- src/components/CounterDown.jsx
+++ src/components/CounterDown.jsx
@@ -1,22 +1,22 @@
 import { useCountdown } from '../hooks/useCounterDown'
 
 export function CounterDown({ i18n }) {
-  const { days, hours, minutes, seconds } = useCountdown('5 sept 2024 3:00:00')
+  const { days, hours, minutes, seconds } = useCountdown('5 sept 2024 00:00:00')
   return (
-    <div className='text-white bg-transparent bg-opacity-50 backdrop-blur-md flex text-center justify-center gap-3 text-sm font-semibold py-5 px-5 border rounded-lg mt-4 w-full max-w-[500px]'>
-      <div>
+    <div className='text-black bg-transparent grid grid-cols-2 md:grid-cols-4 gap-4 text-sm md:text-lg font-semibold p-5 border rounded-lg mt-4 w-full'>
+      <div className='flex flex-col items-center'>
         <span className='font-extrabold  text-4xl lg:text-5xl'>{days}</span>{' '}
         {i18n.days}
       </div>
-      <div>
+      <div className='flex flex-col items-center'>
         <span className='font-extrabold text-4xl lg:text-5xl'>{hours}</span>{' '}
         {i18n.hours}
       </div>
-      <div>
+      <div className='flex flex-col items-center'>
         <span className='font-extrabold text-4xl lg:text-5xl'>{minutes}</span>{' '}
         {i18n.minutes}
       </div>
-      <div>
+      <div className='flex flex-col items-center'>
         <span className='font-extrabold text-4xl lg:text-5xl'>{seconds}</span>{' '}
         {i18n.seconds}
       </div>
