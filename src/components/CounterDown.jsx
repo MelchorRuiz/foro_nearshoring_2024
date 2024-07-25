0469b6c89e7266aac113dd9eda4aab8933a38b00
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-07-25 16:10:26 -0600

    refactor: Update CounterDown component to use a different target date

--- src/components/CounterDown.jsx
+++ src/components/CounterDown.jsx
@@ -1,7 +1,7 @@
 import { useCountdown } from '../hooks/useCounterDown'
 
 export function CounterDown({ i18n }) {
-  const { days, hours, minutes, seconds } = useCountdown('5 sept 2024 00:00:00')
+  const { days, hours, minutes, seconds } = useCountdown('5 sept 2024 10:00:00')
   return (
     <div className='text-black bg-transparent grid grid-cols-2 md:grid-cols-4 gap-4 text-sm md:text-lg font-semibold p-5 border rounded-lg mt-4 w-full'>
       <div className='flex flex-col items-center'>
