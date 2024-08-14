339226038b383886ddab0af6da7d88ac701b7e0a
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-08-14 15:56:49 -0600

    update program

--- src/data/program.ts
+++ src/data/program.ts
@@ -1,14 +1,9 @@
 interface Speaker {
+    id: number;
     name: string;
     photo: string;
-    profile: string;
+    profile?: string;
     position: string;
-    social: {
-        facebook?: string;
-        instagram?: string;
-        linkedin?: string;
-        twitter?: string;
-    }
 }
 
 interface Program {
@@ -20,21 +15,15 @@ interface Program {
 
 /*
     {
-        "name": "Inaguración",
-        "description": "Palabras de bienvenida y presentación del foro",
-        "hours": "09:55 hrs",
-        "speakers": [
+        name: "Inaguración",
+        description: "Palabras de bienvenida y presentación del foro",
+        hours: "09:55 hrs",
+        speakers: [
             {
-                "name": "Ricardo Haneine",
-                "photo": "https://via.placeholder.com/150",
-                "profile": "Es Ingeniero en Sistemas Computacionales por la Universidad de las Américas Puebla, Maestro en Ciencias en Ingeniería de Software por el Instituto Tecnológico de Estudios Superiores de Monterrey y Doctor en Ciencias de la Computación por la Universidad de las Américas Puebla. Actualmente es Director de la Facultad de Ingeniería de la Universidad de las Américas Puebla y profesor de tiempo completo en el Departamento de Ciencias de la Computación. Sus áreas de interés son la ingeniería de software, la calidad de software, la educación en ingeniería de software y la educación en línea.",
-                "position": "Director de la Facultad de Ingeniería",
-                "social": {
-                    "facebook": "https://www.facebook.com/ricardo.haneine",
-                    "instagram": "https://www.instagram.com/ricardo.haneine",
-                    "linkedin": "https://www.linkedin.com/in/ricardo-haneine",
-                    "twitter": "https://twitter.com/ricardo.haneine"
-                }
+                name: "Ricardo Haneine",
+                photo: "https://via.placeholder.com/150",
+                profile: "Es Ingeniero en Sistemas Computacionales por la Universidad de las Américas Puebla, Maestro en Ciencias en Ingeniería de Software por el Instituto Tecnológico de Estudios Superiores de Monterrey y Doctor en Ciencias de la Computación por la Universidad de las Américas Puebla. Actualmente es Director de la Facultad de Ingeniería de la Universidad de las Américas Puebla y profesor de tiempo completo en el Departamento de Ciencias de la Computación. Sus áreas de interés son la ingeniería de software, la calidad de software, la educación en ingeniería de software y la educación en línea.",
+                position: "Director de la Facultad de Ingeniería",
             }
         ]
         
@@ -43,37 +32,67 @@ interface Program {
 
 const program : Program[] = [
     {
-        "name": "Inauguración del Foro y palabras de bienvenida",
-        "hours": "10:00 - 10:30 Hrs",
+        name: "Inauguración del Foro y palabras de bienvenida",
+        hours: "10:00 - 10:30 Hrs",
     },
     {
-        "name": "El rol de la geopolítica en el Nearshoring: oportunidades y desafíos del T-MEC como herramientas para el desarrollo regional",
-        "description": "La sesión analizará cómo las dinámicas geopolíticas impactan el Nearshoring, una práctica en crecimiento que permite a las empresas reducir costos y mejorar la eficiencia al acercar sus operaciones a mercados clave.",
-        "hours": "10:30 - 11:30 Hrs",
+        name: "El rol de la geopolítica en el Nearshoring: oportunidades y desafíos del T-MEC como herramientas para el desarrollo regional",
+        description: "La sesión analizará cómo las dinámicas geopolíticas impactan el Nearshoring, una práctica en crecimiento que permite a las empresas reducir costos y mejorar la eficiencia al acercar sus operaciones a mercados clave.",
+        hours: "10:30 - 11:30 Hrs",
+        speakers: [
+            {
+                id: 1,
+                name: "Miguel Leaman Rivas",
+                photo: "/speakers/miguel_angel_leaman.webp",
+                position: "Presidente Ejecutivo de Agencia Por México",
+            },
+        ]
     },
     {
-        "name": "Retos y soluciones en la logística e infraestructura para el Nearshoring",
-        "description": "Esta conversación proporcionará una plataforma para el intercambio de ideas y experiencias, fomentando la colaboración y el desarrollo de soluciones prácticas para los desafíos logísticos e infraestructurales del Nearshoring.",
-        "hours": "11:30 - 12:30 Hrs",
+        name: "Retos y soluciones en la logística e infraestructura para el Nearshoring",
+        description: "Esta conversación proporcionará una plataforma para el intercambio de ideas y experiencias, fomentando la colaboración y el desarrollo de soluciones prácticas para los desafíos logísticos e infraestructurales del Nearshoring.",
+        hours: "11:30 - 12:30 Hrs",
     },
     {
-        "name": "Break",
-        "hours": "12:30 - 12:40 Hrs",
+        name: "Break",
+        hours: "12:30 - 12:40 Hrs",
     },
     {
-        "name": "Innovación y avances en la transformación digital: el papel de la tecnología en el Nearshoring",
-        "description": "La transformación digital está revolucionando el Nearshoring al redefinir la eficiencia y optimizar las cadenas de suministro. Únete a nosotros para descubrir cómo la tecnología está impulsando esta evolución y aprovecha nuestro entorno innovador para intercambiar ideas y desarrollar estrategias.",
-        "hours": "12:40 - 13:40 Hrs",
+        name: "Innovación y avances en la transformación digital: el papel de la tecnología en el Nearshoring",
+        description: "La transformación digital está revolucionando el Nearshoring al redefinir la eficiencia y optimizar las cadenas de suministro. Únete a nosotros para descubrir cómo la tecnología está impulsando esta evolución y aprovecha nuestro entorno innovador para intercambiar ideas y desarrollar estrategias.",
+        hours: "12:40 - 13:40 Hrs",
+        speakers: [
+            {
+                id: 2,
+                name: "Adriana Pulido Sandoval",
+                photo: "/speakers/adriana_pulido_sandoval.webp",
+                position: "CEO de Ilunka",
+            },
+            {
+                id: 3,
+                name: "Edgar Guzman Moreno",
+                photo: "/speakers/edgar_guzman_moreno.webp",
+                position: "Director de la Carrera de Negocios Internacionales I T E S M - Campus León",
+            },
+        ]
     },
     {
-        "name": "Estrategias exitosas, lecciones aprendidas y mejores prácticas en Nearshoring",
-        "description": "Descubre cómo las empresas están transformando sus operaciones al acercar la producción y los servicios a los mercados finales. Obtén valiosos conocimientos para perfeccionar tus prácticas y potenciar tu competitividad en un entorno dinámico.",
-        "hours": "13:40 - 14:40 Hrs",
+        name: "Estrategias exitosas, lecciones aprendidas y mejores prácticas en Nearshoring",
+        description: "Descubre cómo las empresas están transformando sus operaciones al acercar la producción y los servicios a los mercados finales. Obtén valiosos conocimientos para perfeccionar tus prácticas y potenciar tu competitividad en un entorno dinámico.",
+        hours: "13:40 - 14:40 Hrs",
     },
     {
-        "name": "El futuro del Nearshoring: tendencias y proyecciones",
-        "description": "Sumérgete en el futuro del Nearshoring y conoce las tendencias y proyecciones que definirán su evolución. Únete a nuestra sesión para descubrir cómo anticipar cambios, aprovechar nuevas oportunidades y optimizar tus operaciones al máximo. ",
-        "hours": "14:40 - 15:40 Hrs",
+        name: "El futuro del Nearshoring: tendencias y proyecciones",
+        description: "Sumérgete en el futuro del Nearshoring y conoce las tendencias y proyecciones que definirán su evolución. Únete a nuestra sesión para descubrir cómo anticipar cambios, aprovechar nuevas oportunidades y optimizar tus operaciones al máximo. ",
+        hours: "14:40 - 15:40 Hrs",
+        speakers: [
+            {
+                id: 4,
+                name: "Ari - Ben Saks González",
+                photo: "/speakers/ari_ben_saks.webp",
+                position: "Ernst & Young Global Specialist",
+            },
+        ]
     },
 ]
 
